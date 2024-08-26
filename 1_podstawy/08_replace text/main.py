d866302efad4a04a328b9955c874be1cf94d04a0
bulandajakub <bulandajakub@gmail.com>
2024-08-26 15:59:49 +0200

    Add function which replaces all occurences of target string in mainText with repl string.

---
+++ 1_podstawy/08_replace text/main.py
@@ -0,0 +1,13 @@
+def replace_all(mainText: str, target: str, repl: str) -> str:
+    """Replaces all occurrences of target string in mainText with repl string.
+    
+    Args:
+        mainText (str): The string in which to replace the target.
+        target (str): The string to replace.
+        repl (str): The string to use for replacement.
+    
+    Returns:
+        str: The new string with the replacements made.
+    """
+    replaced = mainText.replace(target, repl)
+    return replaced
--- 1_podstawy/main.py
+++ 1_podstawy/main.py
@@ -1,20 +1,7 @@
-def convert_emojis(input_message: str) -> str:
-    """
-    Convert emoticons in input message to their corresponding emojis.
-
-    Args:
-        input_message (str): The input message to be converted.
-
-    Returns:
-        str: The converted message with emoticons replaced by emojis.
-    """
-    emoji_dict = {
-        ":)": "😊",
-        ":(": "😔"
-    }
-    converted_message = ' '.join(
-        emoji_dict.get(word, word) for word in input_message.split())
-    return converted_message
-
-text = input("Enter a message: ")
-print(convert_emojis(text))
+secrete = 'tramsgferagfuoy'
+#1. Replace all the of 'gf' with ' ' in the secrete text
+secrete = secrete.replace('gf', ' ')
+#2. Reverse the String
+secrete = secrete[::-1]
+#3. Display the result
+print(secrete)
