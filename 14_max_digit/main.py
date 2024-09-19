a6e918209faaf1b9038c9890e74acc85a802e405
bulandajakub <bulandajakub@gmail.com>
2024-09-19 15:52:11 +0200

    Add function which returns the biggest number of a string

--- 14_max_digit/main.py
+++ 14_max_digit/main.py
@@ -1,2 +1,11 @@
 def max_digit(value: int) -> int:
-    pass
+    """
+    Given an integer value, this function returns the maximum digit in the integer.
+
+    Parameters:
+        value (int): The integer value for which to find the maximum digit.
+
+    Returns:
+        int: The maximum digit in the given integer.
+    """
+    return int(max(list(str(value))))
