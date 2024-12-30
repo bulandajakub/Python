500e0a0901044e906ae3e60fdf4e083b86023274
bulandajakub <bulandajakub@gmail.com>
2024-12-30 15:46:13 +0100

    Add function which check if a string contains three consecutive words.

---
+++ 18_three-words/main.py
@@ -0,0 +1,22 @@
+def three_words(text: str) -> bool:
+    """Check if a string contains three consecutive words.
+
+    Args:
+        text (str): The input string to be checked.
+
+    Returns:
+        bool: True if the string contains three consecutive words, False otherwise.
+    """
+    count = 0
+    # Split the input text into words and iterate over them
+    for word in text.split():
+        # Increment count if the word is alphabetic, otherwise reset to 0
+        count = count + 1 if word.isalpha() else 0
+        # Return True if there are three consecutive alphabetic words
+        if count == 3:
+            return True
+
+assert three_words("Hello World hello") == True
+assert three_words("He is 123 man") == False
+assert three_words("1 2 3 4") == False
+assert three_words("bla bla bla bla") == True
